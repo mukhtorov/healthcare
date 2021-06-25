769b60c54fd1246be1e133517afe122102a5a719
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-06-25 07:54:48 +0500

    Recomadation is ready

---
+++ src/components/PriceCard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Header} from './style'
+import {service} from '../../mock/service'
+
+
+export default ()=> {
+    return (
+       <Header>
+
+       </Header>
+    )
+}
--- src/views/Generic.js
+++ src/views/Generic.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import List from '../components/DoctorsList'
 import Price from '../components/Price'
-
+import Recomadation from '../components/Recomadation'
 export default (props) => {
     return (
         <h1>
             <List />
             <Price />
+            <Recomadation/>
             Coming Soon {props.location.pathname }
         </h1>
     )
